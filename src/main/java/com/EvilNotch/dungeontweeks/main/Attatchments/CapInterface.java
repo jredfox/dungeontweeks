1f7f07d53c0d5ed68a5ecafc367841caef6add32
jredfox <jjredfox@hotmail.com>
2018-09-21 06:54:23 -0500

    refractoring

--- src/main/java/com/EvilNotch/dungeontweeks/main/Attatchments/CapInterface.java
+++
@@ -1,8 +0,0 @@
-package com.EvilNotch.dungeontweeks.main.Attatchments;
-
-public interface CapInterface {
-	
-	public boolean getScanned();
-	public boolean setScanned(boolean b);
-
-}
--- src/main/java/com/EvilNotch/dungeontweeks/newAttatch/ICapabilityMagic.java
+++
@@ -1,7 +0,0 @@
-package com.EvilNotch.dungeontweeks.newAttatch;
-
-public interface ICapabilityMagic extends ICapabilityBase{
-	public int getMana();
-	public void setMana(int amount);
-
-}
